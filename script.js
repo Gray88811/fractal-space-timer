c18b55760401c5216b15b3ee9ee1584557866556
Gray88811 <72987745+Gray88811@users.noreply.github.com>
2022-05-29 19:53:23 +0300

    Initial Release

---
+++ script.js
@@ -0,0 +1,23 @@
+// Set the date we're counting down to
+var countDownDate = new Date("Mar 23, 2019 16:25:58").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = now - countDownDate;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+}, 1000);
